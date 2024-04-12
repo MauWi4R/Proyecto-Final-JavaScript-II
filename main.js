b915601c365a4bdafbfd59aa61b4a355ad5ad486
Ivy Barron <124299092+MauWi4R@users.noreply.github.com>
2024-04-12 01:05:53 -0600

    main.js

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ document.addEventListener("DOMContentLoaded", function() {
         to = document.getElementById("to").value;
         date = document.getElementById("dateRange").value;
 
-        url ='https://my-json-server.typicode.com/MauWi4R/api.airlines/db/';
+        url ='http://localhost:3000/';
         if (aerolinea !== "all") { // Si no es la opción comodín, añade la aerolínea a la URL
           url += aerolinea;
         }
